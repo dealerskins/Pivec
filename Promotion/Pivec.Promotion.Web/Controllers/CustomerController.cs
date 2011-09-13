afb031f3ee6eee49417db5260ab84294cd97f0b5
Carlos Chacon <carlos.chacon@proximitycr.com>
2011-09-13 09:46:52 -0600

    Error Handling.

--- Promotion/Pivec.Promotion.Web/Controllers/CustomerController.cs
+++ Promotion/Pivec.Promotion.Web/Controllers/CustomerController.cs
@@ -39,26 +39,34 @@ namespace Pivec.Promotion.Web.Controllers
             {
                 // Create the hashcode for the license driver that will be saved in the database.
                 string licenseHashCode = customer.DriverLicenseNumber.GetHashCode().ToString();
+                ViewData.Clear();
 
-                if (IsUserRegistered(licenseHashCode))
+                Nullable<Guid> promotionId = GetActivePromotionId();
+
+                if (!promotionId.HasValue)
                 {
-                    // If the user is registered for the promotion return an error message.
-                    return RedirectToRoute("Error",
-                                           new
-                                               {
-                                                   errorMessage =
-                                               "The customer has already been added to the promotion"
-                                               });
-
+                    ViewData.Add(new KeyValuePair<string, object>("PromotionError", true));
+                    ViewData.Add(new KeyValuePair<string, object>("ErrorMessage", "There are no active promotions."));
+                    ViewBag.DealerId = null;
+                    ViewBag.DealerId = new SelectList(db.Dealers, "Id", "Name", customer.DealerId);
+                    return View(customer);
                 }
 
+                if (IsUserRegistered(licenseHashCode, promotionId.Value))
+                {
+                    ViewBag.DealerId = new SelectList(db.Dealers, "Id", "Name", customer.DealerId);
+                    ViewData.Add(new KeyValuePair<string, object>("LicenseError", true));
+                    ViewData.Add(new KeyValuePair<string, object>("ErrorMessage", String.Format("{0} {1} has already been included in the current promotion", customer.FirstName, customer.LastName)));
+                    return View(customer);
+                }
+                
                 customer.Id = Guid.NewGuid();
                 customer.DateCreated = DateTime.UtcNow;
-                customer.PromotionId = GetActivePromotionId();
+                customer.PromotionId = promotionId.Value;
                 customer.DriverLicenseNumber = customer.DriverLicenseNumber.GetHashCode().ToString();
                 db.Customers.AddObject(customer);
                 db.SaveChanges();
-                SendThankYouEmail(customer.Email);
+                SendThankYouEmail(customer.Email, customer.FirstName, customer.LastName);
 
                 // Logs off the user and returns them to the Login Page.
                 return RedirectToAction("LogOff", "Account");
@@ -72,7 +80,7 @@ namespace Pivec.Promotion.Web.Controllers
         /// Sends an email based on the options set in the webconfig file.
         /// </summary>
         /// <param name="toEmailAddress">The customers email address</param>
-        private void SendThankYouEmail(string toEmailAddress)
+        private void SendThankYouEmail(string toEmailAddress, string firstName, string lastName)
         {
             // Create the mail message.
             MailMessage mail = new MailMessage();
@@ -83,7 +91,7 @@ namespace Pivec.Promotion.Web.Controllers
 
             // Set the content.
             mail.Subject = ConfigurationManager.AppSettings["EmailSubject"];
-            mail.Body = ConfigurationManager.AppSettings["EmailBody"];
+            mail.Body = string.Format(ConfigurationManager.AppSettings["EmailBody"], firstName, lastName);
 
             // Send the message.
             SmtpClient smtp = new SmtpClient(ConfigurationManager.AppSettings["SMTPServer"]);
@@ -94,7 +102,7 @@ namespace Pivec.Promotion.Web.Controllers
         /// Gets the promotion that is currently available.
         /// </summary>
         /// <returns>The id the promotion</returns>
-        private Guid GetActivePromotionId()
+        private Nullable<Guid> GetActivePromotionId()
         {
             var promotionId = (from p in db.Promotions
                                where p.IsActive == true
@@ -107,10 +115,10 @@ namespace Pivec.Promotion.Web.Controllers
         /// Checks if the user is already registered for the current promotion.
         /// </summary>
         /// <param name="licenseNumber">The license number to check.</param>
+        /// <param name="promotionId">The id of the promotion</param>
         /// <returns>True if the user is already registered to the actual promotion.</returns>
-        private bool IsUserRegistered(string licenseNumber)
+        private bool IsUserRegistered(string licenseNumber, Guid promotionId)
         {
-            Guid promotionId = GetActivePromotionId();
             var userRegistered = (from c in db.Customers
                                       where
                                           c.DriverLicenseNumber.Equals(licenseNumber)
