a6e8b4330ae875759ab6fddd3bf923a0e8d1f5b0
Andreas Veuthey <aveuthey@proximitycr.com>
2011-09-14 11:09:04 -0600

    - Adding filtering logic to the customer search view - Adding export to Excel logic to the customer search view

---
+++ Promotion/Pivec.Promotion.Web/ViewModels/CustomerViewModels.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+using Pivec.Promotion.Data;
+
+namespace Pivec.Promotion.Web.ViewModels
+{
+
+    public class CustomerSearchViewModel
+    {
+        [Display(Name = "Dealer")]
+        public SelectList DealerId { get; set; }
+
+        [Display(Name = "Salesperson Code")]
+        [RegularExpression(@"^[a-zA-Z0-9\-]*$", ErrorMessage = "Sales person code is invalid.")]
+        public string SalespersonCode { get; set; }
+
+        [Display(Name = "Date From")]
+        [DataType(DataType.Date, ErrorMessage = "Date From is invalid.")]
+        public DateTime? DateFrom { get; set; }
+
+        [Display(Name = "Date To")]
+        [DataType(DataType.Date, ErrorMessage = "Date To is invalid.")]
+        public DateTime? DateTo { get; set; }
+
+        public IEnumerable<Customer> Customers { get; set; }
+    }
+
+    public class CustomerExcelViewModel
+    {
+        public string fileName { get; set; }
+        public IEnumerable<Customer> contents { get; set; }
+    }
+
+}
---
+++ Promotion/Pivec.Promotion.Web/ViewModels/ExcelBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pivec.Promotion.Data
+{
+    public class ExcelBase
+    {
+        public string fileName { get; set; }
+        public List<Customer> contents { get; set; }
+    }
+}
